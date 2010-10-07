9d51495dbed4dd35db6e4ac0e47ba53cae9f721a
rodney.amato <rodney.amato@interspire.com>
2010-10-08 10:54:20 +1100

    Add more boilerplate for the initial setup of djbdns to make it more automatic

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,15 +1,77 @@
 class tinydns::setup {
-  exec { "rebuild-tinydns-data":
-    cwd => "/etc/tinydns/root",
-    command => "/usr/bin/make",
-    refreshonly => true
-  }
+    exec { "rebuild-tinydns-data":
+        cwd => "/etc/tinydns/root",
+        command => "/usr/bin/make",
+        refreshonly => true,
+        require => [Package["djbdns"], Exec["tinydns-setup"], Exec["dnscache-setup"]],
+        notify => Service["dnscache"]
+    }
 
-  package { "djbdns":
-    ensure => "present"
-  }
+    exec { "tinydns-setup":
+        command => "/usr/bin/tinydns-conf tinydns dnslog /etc/tinydns 127.0.0.1",
+        creates => "/etc/tinydns",
+        require => Package["djbdns"]
+    }
 
-  package { "make":
-    ensure => "present"
-  }
+    exec { "dnscache-setup":
+        command => "/usr/bin/dnscache-conf dnscache dnslog /etc/dnscache $ipaddress",
+        creates => "/etc/dnscache",
+        require => Package["djbdns"]
+    }
+
+    user { "tinydns":
+        ensure => present,
+        comment => "Tinydns User",
+        home => "/dev/null",
+        shell => "/bin/false",
+        uid => 30000,
+    }
+
+    user { "dnscache":
+        ensure => present,
+        comment => "Dnscache User",
+        home => "/dev/null",
+        shell => "/bin/false",
+        uid => 30001,
+    }
+
+    user { "dnslog":
+        ensure => present,
+        comment => "Djbdns Log User",
+        home => "/dev/null",
+        shell => "/bin/false",
+        uid => 30002,
+    }
+
+    package { "djbdns":
+        ensure => "present"
+    }
+
+    package { "make":
+        ensure => "present"
+    }
+
+    file { "/etc/service/tinydns":
+        ensure => "/etc/tinydns",
+        require => [Exec["tinydns-setup"], Exec["dnscache-setup"]]
+    }
+
+    file { "/etc/service/dnscache":
+        ensure => "/etc/dnscache",
+        require => [Exec["tinydns-setup"], Exec["dnscache-setup"]]
+    }
+
+    file { "/etc/dnscache/root/servers/$domain":
+        content => "127.0.0.1"
+    }
+
+    service { "dnscache":
+        provider => "daemontools",
+        path => "/etc/dnscache";
+    }
+
+    service { "tinydns":
+        provider => "daemontools",
+        path => "/etc/dnscache";
+    }
 }
